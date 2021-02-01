f871bbc88f8175fa60947750cdf36b5ff806060b
Austin.MacbookPro <bellin83@gmail.com>
2021-02-02 00:09:43 +0900

    검색 영역 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css"
+import SearchInput from "../components/SearchInput/SearchInput";
 
 export default function Home({countries}) {
   console.log(countries)
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length}</div>
-      <SearchInput />
+      <SearchInput placeholder="이름이나 지역을 입력하세요." />
     </Layout>
   );
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch("https://rescountries.eu/rest/v2/all");
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
   const countries = await res.json();
 
   return {
