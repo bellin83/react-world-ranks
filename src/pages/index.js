a1e9dae138c88f51f6aed375ecd01bec46c0c6ef
Austin.MacbookPro <bellin83@gmail.com>
2021-02-03 00:05:09 +0900

    필터 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,35 @@ import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css"
 import SearchInput from "../components/SearchInput/SearchInput";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
+import {useState} from "react";
 
 export default function Home({countries}) {
   console.log(countries)
+
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(country =>
+    country.name.toLowerCase().includes(keyword) ||
+    country.region.toLowerCase().includes(keyword) ||
+    country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  }
+
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length}</div>
 
-      <SearchInput placeholder="이름이나 지역을 입력하세요." />
+      <SearchInput
+        placeholder="이름이나 지역을 입력하세요."
+        onChange={onInputChange}
+      />
 
-      <CountriesTable countries={countries} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
